cd718bca165a801e900ea552a41fb9836007b2ed
Eden <eden.alemayehut@aastustudent.edu.et>
2026-04-07 14:26:20 +0300

    Add solution for H-Index II

---
+++ leetcode/0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        res = float('-inf')
+        def binary(n):
+            low, high = 0, len(citations)
+            while low <= high:
+                mid = (low + high)//2
+                print(mid)
+                if mid < len(citations) and citations[mid] >= n:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return len(citations) - high - 1
+        for i in range(0, len(citations)+1):
+            amt = binary(i)
+            if amt >= i:
+                res = max(res, i)
+        return res
+            
