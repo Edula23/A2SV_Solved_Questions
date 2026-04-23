ba43db73b5d295317140f3f11bc420497d19d9c7
Eden <eden.alemayehut@aastustudent.edu.et>
2026-04-23 13:56:45 +0300

    Add solution for Surrounded Regions

---
+++ leetcode/0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows, cols = len(board), len(board[0])
+        def unsurrounded(r, c):
+            if (r < 0 or c < 0 or r==rows or c == cols or board[r][c] != 'O'):
+                return
+            board[r][c] = 'T'
+            unsurrounded(r+1, c)
+            unsurrounded(r-1, c)
+            unsurrounded(r, c+1)
+            unsurrounded(r, c-1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O' and r in [0, rows-1] or c in [0, cols-1]:
+                    unsurrounded(r, c)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+
+        
