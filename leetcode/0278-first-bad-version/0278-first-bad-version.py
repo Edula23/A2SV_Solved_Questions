4863c6b50651e46c248b232b67399af52c52e4a5
Eden Alemayehu <eden.alemayehut@aastustudent.edu.et>
2026-06-27 07:03:57 +0300

    Add solution for First Bad Version

--- leetcode/0278-first-bad-version/0278-first-bad-version.py
+++ leetcode/0278-first-bad-version/0278-first-bad-version.py
@@ -7,11 +7,10 @@ class Solution:
         prev = False
         while low <= high:
             mid = (low + high)//2
-            if not isBadVersion(mid):
-                low = mid+1
-            else:
-                if isBadVersion(mid) and not isBadVersion(mid-1):
-                    return mid
+            if isBadVersion(mid):
                 high = mid - 1
+            else:
+                low = mid + 1
+        return low
 
 
