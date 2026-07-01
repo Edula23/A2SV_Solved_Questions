1d1ed39d4cddb30fe3e727cdc06ee192f534bf8f
Eden Alemayehu <eden.alemayehut@aastustudent.edu.et>
2026-07-01 10:35:40 +0300

    Add solution for Successful Pairs of Spells and Potions

---
+++ leetcode/2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,17 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        pairs = [0] * len(spells)
+        def noSuccess(n):
+            low, high = 0, len(potions)-1
+            while low <= high:
+                mid = (low + high)//2
+                if potions[mid] * n < success:
+                    low = mid + 1
+                else:
+                    high = mid-1
+            return len(potions) - high -1
+
+        for i in range(len(spells)):
+            pairs[i] = noSuccess(spells[i])
+        return pairs
