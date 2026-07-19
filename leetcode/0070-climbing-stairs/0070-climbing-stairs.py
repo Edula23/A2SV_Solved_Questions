0fabcc5aced390b20e1762b9525422b04c37207c
Eden Alemayehu <eden.alemayehut@aastustudent.edu.et>
2026-07-19 11:22:14 +0300

    Add solution for Climbing Stairs

---
+++ leetcode/0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def rec(n):
+            if n < 3:
+                return n
+            if n not in memo:
+                memo[n] = rec(n-1) + rec(n-2)
+            return memo[n]
+        return rec(n)
