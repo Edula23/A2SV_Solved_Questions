491566ff77d20d51acb3ab67f9d82683047c9584
Eden Alemayehu <eden.alemayehut@aastustudent.edu.et>
2026-08-10 03:54:57 +0300

    Add solution for Stone Game

--- leetcode/0877-stone-game/0877-stone-game.py
+++ leetcode/0877-stone-game/0877-stone-game.py
@@ -1,16 +1,14 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
-        # def dp(i, j, turn, a, b):
-            
-        #     if i >= len(piles) or j < 0 or j < i:                
-        #         return a > b
-
-        #     if turn == 'a':               
-        #         return dp(i+1, j, 'b', a+piles[i], b) or dp(i, j-1, 'b', a+piles[j], b)
-
-        #     else:
-        #         return dp(i+1, j, 'a', a, b+piles[i]) or dp(i, j-1, 'a', a, b+piles[j])
-
-        
-        # return dp(0, len(piles)-1, 'a', 0, 0)
+        memo = {}
+        def dp(l, r):
+            if l > r:
+                return 0
+            if (l, r) in memo:
+                return memo[(l, r)]
+            even = True if (r-l) % 2 == 0 else False
+            right = piles[r] if even else 0
+            left = piles[l] if even else 0
+            memo[(l, r)] = max(dp(l+1, r)+left , dp(l,r-1)+right)
+            return memo[(l, r)]
+        return (dp(0, len(piles)-1) > sum(piles)//2)
